9a578e601c37f7b21f2b5e40f40000c4f3426376
Deon Curry <122304970+Curryfrom3@users.noreply.github.com>
2023-04-03 13:10:09 -0700

    taking out lang

--- src/App.js
+++ src/App.js
@@ -18,39 +18,39 @@ class App extends Component {
     };
   }
 
-  applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
-    document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
-    this.loadResumeFromPath(resumePath);
-  }
+  // applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
+  //   this.swapCurrentlyActiveLanguage(oppositeLangIconId);
+  //   document.documentElement.lang = pickedLanguage;
+  //   var resumePath =
+  //     document.documentElement.lang === window.$primaryLanguage
+  //       ? `res_primaryLanguage.json`
+  //       : `res_secondaryLanguage.json`;
+  //   this.loadResumeFromPath(resumePath);
+  // }
 
-  swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    var oppositeLangIcon = document.getElementById(oppositeLangIconId);
-    if (oppositeLangIcon) {
-      oppositeLangIcon.removeAttribute("filter", "brightness(40%)");
-    }
-    var pickedLangIcon = document.getElementById(pickedLangIconId);
-    if (pickedLangIcon) {
-      pickedLangIcon.setAttribute("filter", "brightness(40%)");
-    }
-  }
+  // swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
+  //   var pickedLangIconId =
+  //     oppositeLangIconId === window.$primaryLanguageIconId
+  //       ? window.$secondaryLanguageIconId
+  //       : window.$primaryLanguageIconId;
+  //   var oppositeLangIcon = document.getElementById(oppositeLangIconId);
+  //   if (oppositeLangIcon) {
+  //     oppositeLangIcon.removeAttribute("filter", "brightness(40%)");
+  //   }
+  //   var pickedLangIcon = document.getElementById(pickedLangIconId);
+  //   if (pickedLangIcon) {
+  //     pickedLangIcon.setAttribute("filter", "brightness(40%)");
+  //   }
+  // }
 
 
-  componentDidMount = () => {
-    this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
-  }
+  // componentDidMount = () => {
+  //   this.loadSharedData();
+  //   this.applyPickedLanguage(
+  //     window.$primaryLanguage,
+  //     window.$secondaryLanguageIconId
+  //   );
+  // }
 
   loadResumeFromPath = (path) => {
     $.ajax({
@@ -102,7 +102,7 @@ class App extends Component {
         </Routes>
         <Footer 
           sharedBasicInfo={this.state.sharedData.basic_info}
-          applyPickedLanguage={this.applyPickedLanguage} 
+          // applyPickedLanguage={this.applyPickedLanguage} 
         />  
       </Router>
     );
